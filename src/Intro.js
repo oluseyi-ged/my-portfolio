57905538101400e3dc3401b43f37f674d345fc44
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-07 08:46:47 +0100

    portfolio intro completed

---
+++ src/Intro.js
@@ -0,0 +1,22 @@
+import React from "react"
+import "./Intro.css"
+
+function Intro() {
+  return (
+    <div className="intro">
+      <div className="intro__header">
+        <h1>Hello, I'm Oluseyi Komolafe</h1>
+      </div>
+      <div className="intro__bio">
+        <p>
+          Web Developer currently working as a contract frontend developer at
+          Vodacom on their Markethub initiative. I mostly do front-end
+          development, and i am a React.js and CSS nerd. I am always looking
+          forward to working with and learn from group of other creatives.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Intro
